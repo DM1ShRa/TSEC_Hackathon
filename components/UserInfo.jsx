fbfa8f9c88dff904c1d07ce4c28c6a9d5b9b6d1f
Darash Mishra <darash.mishra@encureit.com>
2024-01-27 17:55:51 +0530

    UI Color

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -8,30 +8,41 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3d3f5b",
+    },
+  },
+});
 
 export default function UserInfo() {
   const { data: session } = useSession();
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {session?.user?.name}
-          </Typography>
-          <Button onClick={() => signOut()} color="inherit">
-            Logout
-          </Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              {session?.user?.name}
+            </Typography>
+            <Button onClick={() => signOut()} color="inherit">
+              Logout
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
   );
 }
